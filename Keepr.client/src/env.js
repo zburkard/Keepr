e11e04a60af7f1d7390da620326936fe33401325
zburkard <burkard.exe@gmail.com>
2023-02-13 23:27:47 -0700

    commit

---
+++ Keepr.client/src/env.js
@@ -0,0 +1,7 @@
+export const dev = window.location.origin.includes('localhost')
+export const baseURL = dev ? 'https://localhost:7045' : ''
+export const useSockets = false
+export const domain = 'zburkard.us.auth0.com'
+export const clientId = 'oribbDAX7rvqmLijDKvEKembEg6GuFwc'
+export const audience = 'https://zak-sandbox.com'
+
